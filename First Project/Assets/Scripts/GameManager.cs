019302e08a5df400344cdcf5c32679a6f6acd8df
alex501212 <56118464+alex501212@users.noreply.github.com>
2020-02-03 17:20:35 +0000

    First commit

---
+++ First Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
