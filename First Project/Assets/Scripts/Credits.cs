747a189d659efe5eabcfa87d24cbfa71ebfd6071
Alex Patterson <56118464+alex501212@users.noreply.github.com>
2025-01-13 15:43:45 +0000

    feat: test title

--- First Project/Assets/Scripts/Credits.cs
+++ First Project/Assets/Scripts/Credits.cs
@@ -1,24 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Credits : MonoBehaviour
-{
-    public void Quit()
-    {
-        Debug.Log("Quit");
-        Application.Quit();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+         Debug.Log("Quit");
+         Application.Quit();
+     }
+-    // Start is called before the first frame update
+     void Start()
+     {
+         
+     }
+-    // Update is called once per frame
+     void Update()
+     {
+         
+-    }
+-}
